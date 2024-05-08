0f6803280caaa8d3c715294f126d15c93ee29414
Kinenystuen <kine_on@hotmail.com>
2024-05-08 14:48:53 +0200

    updated getPosts

--- src/js/api/posts/getPosts.mjs
+++ src/js/api/posts/getPosts.mjs
@@ -1,12 +1,42 @@
-import { apiHostUrl, apiSocial } from "../constants.mjs"
+import { apiSocialUrl } from "../constants.mjs";
 import { headers } from "../headers.mjs";
+import { load } from "../headers.mjs";
+import { createAPIKey } from "../createApiKey.mjs";
 
-export async function userUserProfile() {
-    const response = await fetch(`${apiHostUrl}${apiSocial}`, {
-            headers(),
-    });
+import { displayFeed } from "../../display/feedDisplay.mjs";
+
+const action = "/posts?_author=true&_reactions=true&_comments=true";
+const method = "get";
+const token = load("token");
+
+export async function getPosts(postData) {
+  const getPostsURL = `${apiSocialUrl}${action}`;
+
+  try {
+    // Create API key
+    const { apiKeyData } = await createAPIKey();
+    // console.log("API Key Data:", apiKeyData.data.key);
+
+    // Prepare headers
+    const headersData = headers("application/json");
+    headersData["Authorization"] = `Bearer ${token}`;
+    headersData["X-Noroff-API-Key"] = apiKeyData.data.key;
+
+    const options = {
+      method,
+      headers: headersData,
+      body: JSON.stringify(postData),
+    };
+
+    const response = await fetch(`${getPostsURL}`, options);
+
     if (response.ok) {
-        return await response.json();
+      const posts = await response.json();
+      return posts;
+    } else {
+      throw new Error(`Failed to fetch post: ${response.statusText}`);
     }
-    throw new Error(response.statusText);
+  } catch (error) {
+    console.error("Error fetching post:", error);
+  }
 }
